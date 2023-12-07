90ac58d3b5ec1b873bfaee50bfb83fa21b10213d
Ankith Udupa <ankith.udupa@gmail.com>
2023-12-07 14:45:54 -0500

    pass identifies load instructions to prefetch

--- greedyPrefetchingPass/greedyPrefetching.cpp
+++ greedyPrefetchingPass/greedyPrefetching.cpp
@@ -3,52 +3,71 @@
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Analysis/DependenceAnalysis.h"
-
+#include <optional>
 #include  <iostream>
 
 using namespace llvm;
 
+
 namespace {
 
 struct GreedyPrefetchPass : public PassInfoMixin<GreedyPrefetchPass> {
-
-  bool isDependent(Value *operand, Function *function) {
-    //buggy right now...
-    if (isa<Argument>(operand)) {
-        return true;
+  
+  //determines if a given instruction uses a given pointer value
+  std::optional<Value*> usesValue(Value* instr, Value* val, Value* orig){
+    //if the value is a store instruction we want to check 
+    //if we use the address later
+    if (auto* storeInst = dyn_cast<StoreInst>(val)) {
+      if (storeInst->getValueOperand() == orig || storeInst->getValueOperand() == val){
+        val = storeInst->getPointerOperand();
+      }
     }
 
-    if (auto *inst = dyn_cast<Instruction>(operand)) {
-        if (isa<AllocaInst>(inst)) {
-            // For alloca instructions, check the uses of the alloca
-            for (auto *user : inst->users()) {
-                if (auto *userInst = dyn_cast<Instruction>(user)) {
-                    for (unsigned i = 0; i < userInst->getNumOperands(); ++i) {
-                        if (isDependent(userInst->getOperand(i), function)) {
-                            return true;
-                        }
-                    }
-                }
+    //don't cross function boundaries (maybe consider how this should work properly later)
+    if (auto* callInst = dyn_cast<CallInst>(val)){
+      //errs() << "call inst, skipping. \n";
+      return std::nullopt;
+    }
+    if (auto* retInst = dyn_cast<ReturnInst>(val)){
+      //errs() << "ret inst, skipping. \n";
+      return std::nullopt;
+    }
+
+    //errs() << "instr: " << *instr << ", val: " << *val << "\n";
+
+    for (auto* user : val->users()){
+      //errs() << "   user: " <<  *user << "\n";
+      if (user == instr){
+        if (auto* inst = dyn_cast<Instruction>(user)){
+          for (unsigned i = 0; i < inst->getNumOperands(); ++i){
+            Value *operand = inst->getOperand(i);
+            if (operand == val){
+              errs() << " specific: " << *operand << "\n";
+              return operand;
             }
-        } else {
-            // For other instructions, recursively check each operand
-            for (unsigned i = 0; i < inst->getNumOperands(); ++i) {
-                if (isDependent(inst->getOperand(i), function)) {
-                    return true;
-                }
-            }
+          }
         }
+        //should never reach here
+        errs() << "ruh roh \n"; 
+        return std::nullopt;
+      }
+      auto res = usesValue(instr, user, orig);
+      if (res){
+        return res;
+      }
     }
-
-    return false;
-}
-
-  std::vector<CallInst*> getRecursiveCalls(Function &F) {
+    
+    return std::nullopt;
+  }
+  
+  //returns a vector of call isntructions in a function that are calls to the
+  //same function
+  std::vector<CallInst*> getRecursiveCalls(Function& F) {
     std::vector<CallInst*> res;
-    for (auto bb = F.begin(); bb != F.end(); ++bb){
-      for (auto op  = bb->begin(); op != bb->end(); ++op){
-        if (CallInst *callInst = dyn_cast<CallInst>(op)) {
-          if (Function *calledFunction = callInst->getCalledFunction()) {
+    for (auto& bb: F){
+      for (auto& instr: bb){
+        if (auto* callInst = dyn_cast<CallInst>(&instr)) {
+          if (auto* calledFunction = callInst->getCalledFunction()) {
             if (calledFunction == &F){
               res.push_back(callInst);
             }
@@ -61,44 +80,34 @@ struct GreedyPrefetchPass : public PassInfoMixin<GreedyPrefetchPass> {
 
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
 
-    auto arglist = F.args();
     std::vector<CallInst*> recursiveCalls = getRecursiveCalls(F);
+    //the function isn't recursive so our optimization doesn't work.
     if (recursiveCalls.empty()){
       return PreservedAnalyses::all();
     }
 
-    std::cout << "function " << std::endl;
-    
-    for (auto a = arglist.begin(); a != arglist.end(); ++a){
-      if (PointerType* pt = dyn_cast<PointerType>(a->getType())){
+    auto arglist = F.args();
+    for (auto arg = arglist.begin(); arg != arglist.end(); ++arg){
+      if (auto* pt = dyn_cast<PointerType>(arg->getType())){
         llvm::Type* inner = pt->getPointerElementType();
+        //triggers if the argument is a pointer to a struct
         if (inner->isStructTy()){
           llvm::StructType* st = (llvm::StructType*) inner;
-          std::cout << "struct pointer: " << st->getName().str() << std::endl;
+          
           for (auto* recursiveCall : recursiveCalls){
-            recursiveCall->print(errs());
-            errs() <<"\n"<< "\n";
-            for (unsigned i = 0; i < recursiveCall->getNumOperands(); ++i) {
-                if (isDependent(recursiveCall->getOperand(i), &F)) {
-                    errs() << "Call instruction in function " << F.getName() << " is dependent on its input argument.\n";
-                    recursiveCall->print(errs());
-                    errs() << "\n";
-                    break;
-                }
-              }
+            auto res = usesValue(recursiveCall, arg, arg);
+            if (res){
+              
+              errs() << "Prefetch candidate, arg: " << *arg
+              << ", load to prefetch: " << **res << "\n";
+            }
+            
           }
+          
         }
-        //std::cout << "pointer: " << std::endl;
       }
     }
-    
 
-    // for (auto bb = F.begin(); bb != F.end(); ++bb){
-    //   std::cout << "START basic block with name: " << bb->getName().str() << " in function " << F.getName().str() << std::endl;
-    //   for (auto op = bb->begin(); op != bb->end(); ++op){
-    //     std::cout << "op: " << op->getOpcodeName() << std::endl;
-    //   }
-    // }
     return PreservedAnalyses::all();
   }
 };
--- test.c
+++ test.c
@@ -7,34 +7,20 @@ struct TreeNode {
 };
 
 int foo(struct TreeNode* tree){
-    return foo(tree->l);
+    if (!tree){
+      return 0;
+    }
+    // return foo(tree->l);
+    int x = foo(tree->l) + foo(tree->r);
+    return x + tree->val;
 }
 
-int bar(int y){
-    printf("%d", y);
-    return 2;
-}
 
 int main()
 {
-
-  int in[1000]; 
-  int i,j;
   struct TreeNode n;
   foo(&n);
-  for (i = 0; i < 1000; i++)
-  {
-    in[i] = 0;
-  }   
 
-  for (j = 100; j < 1000; j++)
-  {
-   in[j]+= 10;
-  }
-
-  bar(2023);
-  for (i = 0; i< 1000; i++)
-    fprintf(stdout,"%d\n", in[i]);
   
   return 1;
 }
