73cd1a65511b77b1d6981800b37b51123a8e95bc
Ankith Udupa <ankith.udupa@gmail.com>
2023-12-10 18:56:46 -0500

    much more complicated test + run script automatic correctness checking

--- tests/test1.c
+++ tests/test1.c
@@ -1,31 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Example with a 5-ary Tree
-struct QuintupleTree {
-  int val;
-  struct QuintupleTree* n1;
-  struct QuintupleTree* n2;
-  struct QuintupleTree* n3;
-  struct QuintupleTree* n4;
-  struct QuintupleTree* n5;
-};
+const unsigned int MAX_MESSAGE_SIZE = 1024;
+const unsigned int MAX_NODES = 100000;
 
-void sumPreorder(struct TreeNode* tree){
-    if (!tree){
-      return;
+typedef struct Data {
+  int size;
+  char message[MAX_MESSAGE_SIZE];
+  int flag; //just some data for examples sake
+} Data;
+
+// Example with a Heterogenous k-ary RDS (may contain cycles)
+typedef struct DataNode {
+  struct DataNode* children[10];
+  Data* data;
+  unsigned int index; //for easy visited checking
+  unsigned int numChildren;
+} DataNode;
+
+//almost definitely leaks (unless generated graph has exactly 1 component)
+//its ok though its just an example (just don't use really large n and expect to
+//be able to have lots of free memory afterwards)
+DataNode* getRandomDataLayout(unsigned int n) {
+  DataNode* nodes[n];
+  for (unsigned int i = 0; i < n; ++i){
+    nodes[i] = malloc(sizeof(DataNode));
+    nodes[i]->data = malloc(sizeof(Data));
+    nodes[i]->index = i;
+    nodes[i]->data->size = 1;
+    nodes[i]->data->message[0] = i;
+    nodes[i]->data->flag = i;
+  }
+  for (unsigned int i = 0; i < n; ++i){
+    unsigned int numChildren = 1 + (rand() % 10);
+    for (size_t j = 0; j < numChildren; ++j){
+      unsigned int childNum = rand() % n;
+      nodes[i]->children[j] = nodes[childNum];
     }
-    printf("%d", tree->val);
-    sumPreorder(tree->n1);
-    sumPreorder(tree->n2);
-    sumPreorder(tree->n3);
-    sumPreorder(tree->n4);
-    sumPreorder(tree->n5);
+    nodes[i]->numChildren = numChildren;
+  }
+  return nodes[0];
+}
+
+void traverseDataLayout(DataNode* root, int visited[MAX_NODES]){
+  if (!root){
+    return;
+  }
+  srand(100);
+  printf("%d", root->data->flag);
+  for (size_t i = 0; i < root->numChildren; ++i){
+    if (!visited[root->children[i]->index]){
+      visited[root->children[i]->index] = 1;
+      traverseDataLayout(root->children[i], visited);
+    }
+  }
 }
 
 
 int main()
 {
-  struct QuintupleTree n;
-  sumPreorder(&n);
+  DataNode* dl = getRandomDataLayout(MAX_NODES);
+  int* visited = malloc(sizeof(int)*MAX_NODES);
+  for (unsigned int i = 0; i < MAX_NODES; ++i){
+    visited[i] = 0;
+  }
+  traverseDataLayout(dl, visited);
   return 0;
 }
