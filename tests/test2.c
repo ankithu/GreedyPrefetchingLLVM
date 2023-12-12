1abae6a997f127732522e6db57d20a60b38c42c6
Ankith Udupa <ankith.udupa@gmail.com>
2023-12-12 18:13:37 -0500

    another test

--- tests/test2.c
+++ tests/test2.c
@@ -1,23 +1,51 @@
 #include <stdio.h>
 
-struct TreeNode {
-  int val;
-  struct TreeNode* l;
-  struct TreeNode* r;
-};
+static const unsigned int CHILDREN = 10;
 
-void preorder(struct TreeNode* tree) {
-    if (!tree) {
-      return;
-    }
-    printf("%d", tree->val);
-    preorder(tree->l);
-    preorder(tree->r);
+static const unsigned int PAGE_SIZE = 4096;
+
+typedef struct Node {
+  int val;
+  struct Node* children[CHILDREN];
+} Node;
+
+void leakMemory(unsigned int bytes){
+  malloc(bytes);
 }
 
+Node* buildTree(unsigned int size){
+  Node* q[size + 1];
+  unsigned int tailPtr = 1;
+  q[0] = malloc(sizeof(Node));
+  leakMemory(PAGE_SIZE * 10);
+  for (unsigned int i = 0; i < size; ++i){
+    q[i]->val = i;
+    for (unsigned int j = 0; j < CHILDREN; ++j){
+      q[i]->children[j] = malloc(sizeof(Node));
+      leakMemory(PAGE_SIZE * 10);
+      if (tailPtr < size){
+        q[tailPtr] = q[i]->children[j];
+        ++tailPtr;
+      }
+    }
+  }
+  return q[0];
+}
+
+int sum(Node* root){
+  if (!root){
+    return 0;
+  }
+  int x = root->val;
+  for (unsigned int i = 0; i < CHILDREN; ++i){
+    x += sum(root->children[i]);
+  }
+  return x;
+}
 
 int main() {
-  struct TreeNode n;
-  preorder(&n);
+  Node* tree = buildTree(100000);
+  leakMemory(PAGE_SIZE * 200);
+  printf("%d \n", sum(tree));
   return 0;
 }
